6ad9b50e9e945b8a6ac61dec774eb1f95917adf8
cambeckingsale <cambeckingsale@gmail.com>
2021-09-11 11:32:50 +1000

    never displays > 8 digits, need to deal with the decimal point not showing up until next button clicked

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // TO DO
-// 1. deal with decimal places. This should be dealt with by below
-// 2. deal with overflow / > 8 digits (throw an error)
-//          - this should be rounded for numbers less than 9999,9999 and error otherwise
+// 1. deal with decimal places. not coming up on the screen imediately after being clicked
 // 3 hitting '.' twice should delete the last digit entered (just slice off current input..?). wont work for operators
-
+// 4. add in key presses
+// 5. get digital clock font
+// 6. add in time and day
 
 
 // ------------- START CALCULATOR FUNCTIONS -----------------
@@ -65,30 +65,56 @@ function resetInputs() {
     inputs.b = '0';
     inputs.o = '';
     console.log('cleared');
-    updateCurrent('');
-    updateHistory('');
+    updateCurrent('0');
+    updateHistory('0');
     updateOperator('');
 
 }
 
-function updateCurrent(val) {
-    const currDigits = document.querySelector('.current-digits');
-    if (parseInt(val) < 0) {
-        currDigits.textContent = '-' + String(parseInt(val)*-1).padStart(7, '0');
+function valToDisp(val) {
+    // if val > 9999999 or <-999999 then ERROR
+    // if val < 0, then only 7 digits + '-'
+    // if val is decimal, then only use first 7/8 characters..
+    if (val.slice(val.length -1) === '.') {
+        // do something here to deal with decimal point not coming up...
+    }        
+    val = Number(val)
+    
+
+    if (val > 99999999 | val < -9999999 | val === NaN) {
+        return 'ERRROR'
+    }
+
+    if (val < 0) {
+        if (Number.isInteger(val)) {
+            // number is negative, but no decimal places, so seven digit spots left
+            return '-' + String(val * -1).padStart(7,'0');
+        }
+        else {
+            // number is negative and decimal, so 6 digit spots left
+            return ('-' + String(val * -1).padStart(6,'0')).slice(0,8);
+        }
     }
     else {
-        currDigits.textContent = val.padStart(8,'0');
+        if (Number.isInteger(val)) {
+            // number is positive and no decimal places, so 8 digit spots left
+            return String(val).padStart(8,'0');
+        }
+        else {
+            // number is psoitive and decimal, so 7 digit spots left
+            return String(val).padStart(8,'0').slice(0,8);
+        }
     }
+}
+
+function updateCurrent(val) {
+    const currDigits = document.querySelector('.current-digits');
+    currDigits.textContent = valToDisp(val);
 }
 
 function updateHistory(val) {
     const histDigits = document.querySelector('.history-digits');
-    if (parseInt(val) < 0) {
-        histDigits.textContent = '-' + String(parseInt(val)*-1).padStart(7, '0');
-    }
-    else {
-        histDigits.textContent = val.padStart(8,'0');
-    }
+    histDigits.textContent = valToDisp(val);
 }
 
 function updateOperator(val) {
@@ -99,11 +125,11 @@ function updateOperator(val) {
 function setupButtons() {
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
-        button.addEventListener('click', buttonClick);
+        button.addEventListener('click', inputIn);
     })
 }
 
-function buttonClick(e) {
+function inputIn(e) {
     switch (e.target.className) {
         case 'digit':
             switch (inputs.inputAB) {
@@ -129,18 +155,28 @@ function buttonClick(e) {
         case 'operator':
             switch (inputs.inputAB) {
                 case 'a':
-                    // new operator so save it
-                    inputs.o = e.target.value;
-                    if (inputs.o === '=') {
-                        // if the operator was the equal sign then we clear, as this is our clear button
-                        resetInputs()
+                    //if its a negative sign and 'a' is still 0, then we want 'a' to be -ve
+                    if (e.target.value === '-' & inputs.a == 0) {
+                        inputs.a = '-';
+                        console.log(inputs);
+                        break
                     }
                     else {
-                        inputs.inputAB = 'b'
-                        console.log(inputs);
-                        updateOperator(inputs.o);    
-                    }
+                        // new operator so save it
+                        inputs.o = e.target.value;
+                        if (inputs.o === '=') {
+                            // if the operator was the equal sign then we clear, as this is our clear button
+                            resetInputs()
+                        }
+                        else {
+                            inputs.inputAB = 'b'
+                            console.log(inputs);
+                            updateOperator(inputs.o);   
+                            updateHistory(inputs.a); 
+                            updateCurrent('0');
+                        }
                         break;
+                    }
                 case 'b':
                     // second time an operator has been clicked is always an '=' operation regardless of button
                     let res = operate(inputs.a, inputs.o, inputs.b);
